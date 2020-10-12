a349ea3cbebfb52648204762bc6ee6c62f03d2c1
Bima Kumkum <bimakumkum19@gmail.com>
2020-10-12 13:25:41 +0600

    Add files via upload

---
+++ MyBook.java
@@ -0,0 +1,17 @@
+
+public class MyBook extends Book {
+
+		int price;
+	
+MyBook(String title,String author,int price){
+	super(title,author);
+	this.price = price;
+}
+void display() {
+	
+
+	System.out.println("Title: "+super.title);
+	System.out.println("Author: "+super.author);
+	System.out.println("Price: "+price);
+}
+}
